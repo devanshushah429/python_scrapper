a7c3e247da27497678703f4cc8fddbea2d512281
Devanshu Shah <devanshushah294@gmail.com>
2024-09-17 10:45:01 +0530

    add web_page_fetcher.py

---
+++ util/requests_handler/web_page_fetcher.py
@@ -0,0 +1,18 @@
+import requests
+class WebPageFetcher:
+    def __init__(self, url):
+        self.url = url
+    
+    def get_page_source_by_url_using_requests(self):
+        
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+
+        response = requests.get(self.url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        else:
+            print(f'Failed to retrieve the page. Status code: {response.status_code}')
+            print(response)
+            return None
