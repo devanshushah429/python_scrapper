f580b0e2b765318dfe781ef632a70624cb7e1e6c
Devanshu Shah <devanshushah294@gmail.com>
2024-09-18 09:35:07 +0530

    update web_scrapper.py

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,140 +1,61 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from  util.lxml_handler.xpath_extractor import XPathExtractor
+from util.lxml_handler.html_parser import HTMLParser
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
-from pymongo import MongoClient
 import json
-import re
-from lxml import html
 import logging
+from database.mongo_db_handler import MongoDBHandler
+from util.selenium_handler.selenium_web_page_scrapper import SeleniumWebPageScrapper
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def get_mongo_client(connection_string):
-    return MongoClient(connection_string)
-
-def get_db_and_collection(client, database_name, collection_name):
-    db = client[database_name]
-    collection = db[collection_name]
-    return db, collection
-
-def get_xpath_inner_text(tree, xpath):
+def scrape_details_page(selenium_web_page_scraper: SeleniumWebPageScrapper, url, xpaths):
     try:
-        elements = tree.xpath(xpath)
-        if elements:
-            strings = [element.text_content() for element in elements]
-            result_str = " ".join(strings)
-            return re.sub(r"\s+", " ", result_str).strip()
-        return None
-    except Exception as e:
-        logging.error(f"Error in get_xpath_inner_text: {e}")
-        return None
+        selenium_web_page_scraper.load_url(url)
+        tree = HTMLParser(selenium_web_page_scraper.get_page_source()).parse()
 
-def get_xpath_attribute(tree, xpath, attribute):
-    try:
-        elements = tree.xpath(xpath)
-        if elements:
-            if attribute == "innerHTML":
-                return html.tostring(elements[0], method='html', encoding='unicode')
-            return elements[0].get(attribute)
-        return None
-    except Exception as e:
-        logging.error(f"Error in get_xpath_attribute: {e}")
-        return None
-
-def get_multiple_xpath_attribute(tree, xpath, attribute):
-    try:
-        elements = tree.xpath(xpath)
-        return [element.get(attribute) for element in elements]
-    except Exception as e:
-        logging.error(f"Error in get_multiple_xpath_attribute: {e}")
-        return None
-
-def get_multiple_xpath_inner_text(tree, xpath):
-    try:
-        elements = tree.xpath(xpath)
-        return [re.sub(r"\s+", " ", element.text_content()).strip() for element in elements]
-    except Exception as e:
-        logging.error(f"Error in get_multiple_xpath_inner_text: {e}")
-        return None
-
-def get_xpath_attribute_from_element(element, xpath=None, attribute=None):
-    try:
-        if not xpath:
-            if attribute == "innerHTML":
-                return html.tostring(element, method='html', encoding='unicode')
-            return element.get(attribute)
-
-        result_elements = element.xpath(xpath)
-
-        if result_elements:
-            return result_elements[0].get(attribute)
-        
-        return None
-    except Exception as e:
-        logging.error(f"Error in get_xpath_attribute_from_element: {e}")
-        return None
-
-def get_xpath_inner_text_from_element(element, xpath):
-    try:
-        if not xpath:
-            return element.text_content().strip()
-        result_elements = element.xpath(xpath)
-        if result_elements:
-            return result_elements[0].text_content().strip()
-        return None
-    except Exception as e:
-        logging.error(f"Error in get_xpath_inner_text_from_element: {e}")
-        return None
-
-def scrape_details_page(driver, url, xpaths):
-    try:
-        driver.get(url)
-        WebDriverWait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
-        tree = html.fromstring(driver.page_source)
-
+        xpath_extractor = XPathExtractor(tree)
         details_data = {}
         for key, xpath in xpaths["details_page"]["inner_text_xpath_dictionary"].items():
-            details_data[key] = get_xpath_inner_text(tree, xpath)
+            details_data[key] = xpath_extractor.get_xpath_inner_text(xpath)
 
         for key, (xpath, attribute) in xpaths["details_page"]["attribute_dictionary"].items():
-            details_data[key] = get_xpath_attribute(tree, xpath, attribute)
+            details_data[key] = xpath_extractor.get_xpath_attribute(xpath, attribute)
 
         for key, (xpath, attribute) in xpaths["details_page"]["multile_attribute_dictionary"].items():
-            details_data[key] = get_multiple_xpath_attribute(tree, xpath, attribute)
+            details_data[key] = xpath_extractor.get_multiple_xpath_attribute(xpath, attribute)
 
         for key, xpath in xpaths["details_page"]["multiple_inner_text_dictionary"].items():
-            details_data[key] = get_multiple_xpath_inner_text(tree, xpath)
+            details_data[key] = xpath_extractor.get_multiple_xpath_inner_text(xpath)
 
         return details_data
     except Exception as e:
         logging.error(f"Error in scrape_details_page: {e}")
         return {}
 
-def fetch_block_data(driver, page_url, xpaths):
+def fetch_block_data(selenium_web_page_scraper:SeleniumWebPageScrapper, page_url, xpaths):
     try:
-        driver.get(page_url)
-        WebDriverWait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
-        tree = html.fromstring(driver.page_source)
+        selenium_web_page_scraper.load_url(page_url)
+        tree = HTMLParser(selenium_web_page_scraper.get_page_source()).parse()
         block_xpath = xpaths["block_xpath"]
         elements = tree.xpath(block_xpath)
 
+        xpath_extractor = XPathExtractor(tree)
         blocks_data = []
         for element in elements:
             try:
                 block_details = {"page_url": page_url}
 
                 for key, value in xpaths["block_details"]["inner_text_xpath_dictionary"].items():
-                    block_details[key] = get_xpath_inner_text_from_element(element, value)
+                    block_details[key] = xpath_extractor.get_xpath_inner_text_from_element(element, value)
 
                 for key, value in xpaths["block_details"]["attribute_dictionary"].items():
                     if key == "url":
-                        block_details[key] = xpaths["base_url"] + get_xpath_attribute_from_element(element, value[0], value[1])
+                        block_details[key] = xpaths["base_url"] + xpath_extractor.get_xpath_attribute_from_element(element, value[0], value[1])
                     else:
-                        block_details[key] = get_xpath_attribute_from_element(element, value[0], value[1])
+                        block_details[key] = xpath_extractor.get_xpath_attribute_from_element(element, value[0], value[1])
 
                 blocks_data.append(block_details)
             except Exception as e:
@@ -146,18 +67,17 @@ def fetch_block_data(driver, page_url, xpaths):
         logging.error(f"Error in fetch_block_data: {e}")
         return []
 
-def process_page(page_url, path_to_chromedriver, xpaths, collection):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    service = Service(path_to_chromedriver)
-    new_driver = webdriver.Chrome(service=service, options=chrome_options)
+def process_page(page_url, path_to_chromedriver, xpaths):
+
+    mongo_db_handler = MongoDBHandler(xpaths["connection_string"], xpaths["database_name"], xpaths["collection_name"])
+    selenium_web_page_scrapper = SeleniumWebPageScrapper(path_to_chromedriver)
 
     try:
-        blocks_data = fetch_block_data(new_driver, page_url, xpaths)
+        blocks_data = fetch_block_data(selenium_web_page_scrapper, page_url, xpaths)
         all_data = []
 
         for block_data in blocks_data:
-            details_page_data = scrape_details_page(new_driver, block_data.get("url"), xpaths)
+            details_page_data = scrape_details_page(selenium_web_page_scrapper, block_data.get("url"), xpaths)
             logging.info(f"Processing block data: {block_data}")
             data = {
                 "time_stamp": datetime.now(),
@@ -170,20 +90,16 @@ def process_page(page_url, path_to_chromedriver, xpaths, collection):
             all_data.append(data)
 
         if all_data:
-            collection.insert_many(all_data)
+            mongo_db_handler.insert_many(all_data)
     except Exception as e:
         logging.error(f"Error in process_page: {e}")
         return all_data
     finally:
-        new_driver.quit()
+        selenium_web_page_scrapper.quit_driver()
 
 def main():
     path_to_chromedriver = "C:\\Program Files\\chromedriver-win64\\chromedriver.exe"
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-
-    service = Service(path_to_chromedriver)
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    selenium_web_page_scrapper = SeleniumWebPageScrapper(path_to_chromedriver)
 
     with open("xpaths.json") as f:
         xpaths = json.load(f)
@@ -191,26 +107,23 @@ def main():
     source_url = xpaths["source_url"]
     next_button_xpath = xpaths["next_button_xpath"]
 
-    client = get_mongo_client(xpaths["connection_string"])
-    db, collection = get_db_and_collection(client, xpaths["database_name"], xpaths["collection_name"])
-
     try:
-        driver.get(source_url)
+        selenium_web_page_scrapper.load_url(source_url)
 
         while True:
-            current_page_url = driver.current_url
+            current_page_url = selenium_web_page_scrapper.get_current_url()
             logging.info(f"Processing page: {current_page_url}")
 
             try:
-                process_page(current_page_url, path_to_chromedriver, xpaths, collection)
+                process_page(current_page_url, path_to_chromedriver, xpaths)
 
-                next_button = WebDriverWait(driver, 10).until(
+                next_button = WebDriverWait(selenium_web_page_scrapper.driver, 10).until(
                     EC.element_to_be_clickable((By.XPATH, next_button_xpath))
                 )
 
-                driver.execute_script("arguments[0].scrollIntoView(true);", next_button)
-                driver.execute_script("arguments[0].click();", next_button)
-                WebDriverWait(driver, 10).until(
+                selenium_web_page_scrapper.driver.execute_script("arguments[0].scrollIntoView(true);", next_button)
+                selenium_web_page_scrapper.driver.execute_script("arguments[0].click();", next_button)
+                WebDriverWait(selenium_web_page_scrapper.driver, 10).until(
                     EC.presence_of_element_located((By.XPATH, "//a[contains(@class,'article')]"))
                 )
 
@@ -219,8 +132,7 @@ def main():
                 break
 
     finally:
-        driver.quit()
-        client.close()
+        selenium_web_page_scrapper.quit_driver()
 
 if __name__ == "__main__":
     main()
